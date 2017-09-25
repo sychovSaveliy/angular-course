f6e67e0bb3de28b668209a85b61b4f444518e61c
SychovSaveliy <savelii.sychov@gettyimages.com>
2017-09-25 14:34:44 +0300

    initial commit, created lection pattern

---
+++ server/index.js
@@ -0,0 +1,9 @@
+let http = require('http');
+let static = require('node-static');
+let file = new static.Server('.');
+
+http.createServer(function(req, res) {
+    file.serve(req, res);
+}).listen(4100);
+
+console.log('Server running on port 4100');
