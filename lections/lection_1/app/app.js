ebfabd75fcb95084fff0d02c61479a54652e3e52
SychovSaveliy <savelii.sychov@gettyimages.com>
2017-09-25 17:04:29 +0300

    v1.0

---
+++ lections/lection_1/app/app.js
@@ -0,0 +1,32 @@
+var myApp = angular.module('myApp', []);
+
+var model = {
+    name: 'Vasya',
+    age: 22,
+    friends: [
+        'Petya',
+        'Katya',
+        'Dusya',
+        // 'Dusya',
+        // 'Dusya'
+    ]
+};
+
+myApp.controller("firstCtrl", function ($scope) {
+    $scope.message = 'Hello';
+
+    $scope.model = model;
+    // $scope.model = angular.copy(model);
+
+
+    $scope.handlerInScope = function() {
+        console.log('Clicked');
+
+        $scope.model = model;
+        // $scope.model = angular.copy(model);
+        console.log($scope.model,  model);
+    };
+
+
+    $scope.variable = true;
+});
