f71edbadea0c4f64ef260f54671c4be1a9f27186
SychovSaveliy <savelii.sychov@gettyimages.com>
2017-09-26 17:10:43 +0300

    add services

--- lections/lection_2/README.md
+++ lections/lection_2/README.md
@@ -8,8 +8,9 @@ Services - factory, service, config, constant, value
 
 ## Basics
 
-`code examples`
 
 ## Links
 
 [Forms](https://docs.angularjs.org/guide/forms)
+
+[Forms #2](http://stepansuvorov.com/blog/2015/06/angularjs-%D0%B8-%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%BA%D0%B0-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-%D1%84%D0%BE%D1%80%D0%BC%D1%8B/)
--- lections/lection_2/app/app.js
+++ lections/lection_2/app/app.js
@@ -5,7 +5,7 @@ var model = {
     email: null
 };
 
-myApp.controller("firstCtrl", function ($scope) {
+myApp.controller("firstCtrl", function ($scope, $service_1, $service_2, $factory_1) {
 
     $scope.reset = function() {
         $scope.user = angular.copy(model);
@@ -18,4 +18,71 @@ myApp.controller("firstCtrl", function ($scope) {
     };
 
     $scope.reset();
+
+
+
+
+
+    //Services call
+
+    $scope.serviceOne = function() {
+        console.log($service_1);
+
+        var res = $service_1.publicMethod(50);
+
+        console.log(res);
+
+        console.log($service_1.getPrivate());
+    };
+
+    $scope.serviceTwo = function() {
+        console.log($service_2);
+    };
+    
+    $scope.factoryOne = function () {
+        console.log($factory_1);
+
+        $factory_1.method(3);
+    }
 });
+
+
+// Services
+myApp.service('$service_1', function () {
+    console.log('service_1, do it!');
+
+    var private = 10;
+
+    this.public = 5;
+
+    this.publicMethod = function(x) {
+        return 10 + x;
+    };
+
+    this.getPrivate = function() {
+        return private;
+    }
+
+});
+
+
+myApp.service('$service_2', function ($service_1) {
+    console.log('service_2, do it!');
+
+    this.data = {
+        foo: "name" + $service_1.getPrivate(),
+        age: 500
+    }
+});
+
+myApp.factory('$factory_1', function () {
+    return {
+        foo: "name",
+        age: 10,
+        method: function(size) {
+            console.log('do it ' + size + ' times');
+        }
+    }
+});
+
+
