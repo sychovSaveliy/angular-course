6d6143d9efbea131c97d94a35876416d2e32d004
SychovSaveliy <savelii.sychov@gettyimages.com>
2017-09-29 18:10:21 +0300

    add digest cycle

--- lections/lection_3/app/app.js
+++ lections/lection_3/app/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller("firstCtrl", function ($scope) {
+myApp.controller("firstCtrl", function ($scope, $timeout) {
     $scope.message = 'Hello';
     $scope.showBtn = false;
 
@@ -12,5 +12,30 @@ myApp.controller("firstCtrl", function ($scope) {
     $scope.doIt = function () {
         $scope.$broadcast('dataFromInput', 100500);
         $scope.showBtn = false;
-    }
+    };
+
+
+
+    // digest cycle
+
+    $scope.valueChange = 10;
+
+    setTimeout(function() {
+        $scope.valueChange = 100;
+    }, 2000);
+
+    // $timeout(function() {
+    //     $scope.valueChange = 200;
+    // }, 2000);
+
+
+
+
+    // with apply
+
+    // setTimeout(function() {
+    //     $scope.valueChange = 300;
+    //
+    //     $scope.$apply();
+    // }, 2000);
 });
