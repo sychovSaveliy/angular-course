e08702de419f6e4c0bc657cf6bbb2b73ea3bf766
SychovSaveliy <savelii.sychov@gettyimages.com>
2017-11-12 10:50:24 +0200

    add mock

---
+++ lwa/mock/index.js
@@ -0,0 +1,38 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let port = 5000;
+let $data = require('./routes/data');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.get("/", function (req, res) {
+    return res.json({ message: "Hello! Its API!" })
+});
+
+app.route('/getTest')
+    .get($data.testGetRequest);
+
+app.route('/postTest')
+    .post($data.testPostRequest);
+
+// my routes
+
+app.route('/menu')
+    .get($data.getMenu);
+
+
+app.listen(port);
+console.log("Mock server listening on port " + port);
+
+module.exports = { app };
